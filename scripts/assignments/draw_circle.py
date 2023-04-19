24d5d105b7ae30f87a5c61162b93d7640290337f
shiblesadik603 <smsadik82@gmial.com>
2023-04-19 15:28:03 +0600

    Added isutrction

--- scripts/assignments/draw_circle.py
+++ scripts/assignments/draw_circle.py
@@ -15,3 +15,10 @@ if __name__=='__main__':
         msg.angular.z= 1.0
         pub.publish(msg)
         rate.sleep()
+
+# akhon code e kono change korle just
+# git add .
+# git commit -m "ki change korsi sei relaeted msg (Ex: fixed issue)"
+# git push
+
+# ei 3 ta command dilei hobe, next time kono change kora hoile segula upload korar jonno
